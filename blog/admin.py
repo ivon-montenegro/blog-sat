9a27eaa9d5c5b26c6fc557d9c3f9114396069176
ivon-montenegro <ivonnymj@gmail.com>
2024-09-28 10:26:51 -0400

    list display and list filter admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title", 
+        "author",
+        "body",
+    )
+    list_filter = (
+        "title", 
+        "author",
+    )
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
