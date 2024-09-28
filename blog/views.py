0df99fe0dc07f9292ba351eb4567a71425c31a7f
ivon-montenegro <ivonnymj@gmail.com>
2024-09-28 11:22:23 -0400

    postlist

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post 
+    template_name = "home.html" 
